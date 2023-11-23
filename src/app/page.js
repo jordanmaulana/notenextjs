e90191b834f4bd92dec9058baf006dc68b712a06
adgojoe <jmaulana@adgorithmics.com>
2023-11-23 22:44:50 +0700

    implement get notes api mas indra

--- src/app/page.js
+++ src/app/page.js
@@ -3,13 +3,24 @@ import { Header } from "@/components/globals/header";
 import { NoteHistory } from "@/components/note_history/NoteHistory";
 import { NotePreview } from "@/components/notes/NotePreview";
 
-export default function Home() {
+async function getNotes() {
+  const res = await fetch(
+    "https://devscale-mockapi.fly.dev/api/collections/notes/records?filter=(user='jordanmaulana25@gmail.com')",
+    { cache: "no-store" }
+  );
+  const data = await res.json();
+  console.log(JSON.stringify(data));
+  return data;
+}
+
+export default async function Page() {
+  const { items } = await getNotes();
   return (
     <div className="flex flex-col min-h-screen max-h-screen">
       <Header></Header>
       <main className="flex flex-1 divide-solid flex-col lg:flex-row divide-x overflow-hidden">
         <div className="flex-1 basis-2/3 overflow-x-hidden overflow-y-auto ">
-          <NotePreview />
+          <NotePreview notes={items} />
         </div>
         <div className="flex-1 basis-1/3 overflow-x-hidden overflow-y-auto">
           <NoteHistory></NoteHistory>
--- src/components/notes/NotePreview.jsx
+++ src/components/notes/NotePreview.jsx
@@ -1,24 +1,19 @@
 "use client";
 
-import { useContext } from "react";
-import { NoteContext } from "../providers/NoteProvider";
 import { NoteCard } from "./NoteCard";
 import { EmptyPlaceholder } from "./EmptyPlaceholder";
 
-export const NotePreview = () => {
-  const { notes } = useContext(NoteContext);
-
+export const NotePreview = ({ notes }) => {
   if (notes.length === 0) return <EmptyPlaceholder></EmptyPlaceholder>;
   return (
     <div className="grid grid-cols-3 gap-6 p-8">
-      {(notes ?? []).map(({ date, title, body, isNew }, index) => {
+      {(notes ?? []).map(({ created, additionalData, content }, index) => {
         return (
           <NoteCard
             key={index}
-            date={date}
-            title={title}
-            content={body}
-            isNew={isNew}
+            date={created}
+            title={JSON.parse(additionalData).title}
+            content={content}
             index={index}
           />
         );
